9b1353d80242920b2d3f3b7f38a1ee7d63f99a8f
Rg12rg34 <rahulguptamca1@gmail.com>
2021-09-17 01:21:54 +0530

    method to compute employee wage

--- src/com/BridgeLabz/Main.java
+++ src/com/BridgeLabz/Main.java
@@ -3,17 +3,49 @@ import java.util.Random;
 
 import java.util.Random;
 public class Main {
+    // Declaring a constant
+    public static final int IS_FULLTIME = 1;
+    public static final int IS_PARTTIME = 2;
+    public static final int Emp_Rate_PerHr = 20;
+    public static final int NO_WORKINGDAYS = 20;
+    public static final int MAX_HRS_MONTH = 100;
+
+    public static int computeWage() {
+        // Declaring the variables
+        int empHrs = 0;
+        int empWage = 0;
+        int totalEmpWage = 0;
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
+        /*
+         * Using random method to generate random numbers 0, 1 and 2
+         */
+        while (totalEmpHrs <= MAX_HRS_MONTH && totalWorkingDays < NO_WORKINGDAYS) {
+            totalWorkingDays++;
+            Random random = new Random();
+            int empCheck = random.nextInt(3);
+            switch (empCheck) {
+                case IS_FULLTIME: // FullTime Employee
+                    empHrs = 8;
+                    break;
+                case IS_PARTTIME: // PartTime Employee
+                    empHrs = 4;
+                    break;
+                default:  // Employee is absent
+                    empHrs = 0;
+            }
+            empWage = Emp_Rate_PerHr * empHrs;
+            totalEmpHrs += empHrs;
+            System.out.println("Day : " + totalWorkingDays + " : Employee worked : " + empHrs + " Hours " +
+                    ", Employee Wage : " + empWage);
+        }
+        totalEmpWage = totalEmpHrs * Emp_Rate_PerHr;
+        System.out.println("Total Wages is : " + totalEmpWage);
+        return totalEmpWage;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-        int IS_FULL_TIME = 1;
-        Random random = new Random();
-        int empCheck = random.nextInt(2);
-        if (empCheck == IS_FULL_TIME){
-            System.out.println("Employee is Present");
-        }
-        else {
-            System.out.println("Employee is Absent");
-        }
+        System.out.println("Welcome To Employee Wage Computation Program");
+        computeWage();
     }
 }
